8453874432fe40ae6596461bcd513089024104ba
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2024-12-03 11:08:58 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,12 +1,18 @@
 {
-    'name': 'Purchase Requisition Extended',
+    'name': 'Purchase Order Extended',
     'version': '1.0',
     'category': 'Purchase',
-    'depends': ['purchase_requisition'],
+    'summary': 'Extensión de órdenes de compra',
+    'depends': [
+        'base',
+        'purchase',
+        'purchase_requisition'
+    ],
     'data': [
         'security/ir.model.access.csv',
-        'views/purchase_requisition_views.xml',
+        'views/multi_orden_views.xml',
     ],
     'installable': True,
     'auto_install': False,
+    'license': 'LGPL-3',
 }
